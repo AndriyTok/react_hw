b9366c34f66f6af8991944017dba69a9f1ed7e53
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-07-04 21:11:15 +0300

    fixed functions

--- src/components/services/api.service.ts
+++ src/components/services/api.service.ts
@@ -1,21 +1,25 @@
 import axios from "axios";
+import {IUser} from "../models/IUser";
+import {IPost} from "../models/IPost";
 
-const getAllUsers = () => {
-        return axios.get('https://dummyjson.com/users')
-        .then(response => response.data.users)
-        .catch(error => {
-            console.error("Error fetching users: ", error);
-            throw error;
-        });
+async function getAllUsers(): Promise<IUser[]> {
+    try {
+        const response = await axios.get('https://dummyjson.com/users');
+        return response.data.users;
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+        throw error;
+    }
 }
 
-const getPostsOfUserById = (id: number) => {
-    return axios.get(`https://dummyjson.com/users/${id}/posts`)
-        .then(response => response.data.posts)
-        .catch(error => {
-            console.error(`Error fetching posts for user with id ${id}: `, error);
-            throw error;
-        });
+const getPostsOfUserById = async (id: number): Promise<IPost[]> => {
+    try {
+        const response = await axios.get(`https://dummyjson.com/users/${id}/posts`);
+        return response.data.posts;
+    } catch (error) {
+        console.error(`Error fetching posts for user with id ${id}: `, error);
+        throw error;
+    }
 }
 
 export { getAllUsers, getPostsOfUserById }
