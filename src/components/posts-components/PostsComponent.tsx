14f9544fa76d141c9ad3015e9d48ec68922debfb
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-07-04 21:26:43 +0300

    added a new component PostComponent.tsx

--- src/components/posts-components/PostsComponent.tsx
+++ src/components/posts-components/PostsComponent.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {IPost} from "../models/IPost";
 import styles from './PostsComponent.module.css'
+import PostComponent from "../PostComponent/PostComponent";
 import {useRef, useEffect} from "react";
 
 interface IProps {
@@ -28,16 +29,8 @@ const PostsComponent:FC<IProps> = ({posts, loading}) => {
     return (
        <div ref={postsRef}>
         <ul className={styles.posts_ul}>
-            {posts.map(value => (
-                <ul key={value.id}>
-                    <li>ID: {value.id}</li>
-                    <li>Title: {value.title}</li>
-                    <li>Body: {value.body}</li>
-                    <li>Tags: {value.tags.join(', ')}</li>
-                    <li>Reactions: Likes - {value.reactions.likes}, Dislikes - {value.reactions.dislikes}</li>
-                    <li>Views: {value.views}</li>
-                    <li>User ID: {value.userId}</li>
-                </ul>
+            {posts.map(post => (
+                <PostComponent key={post.id} post={post}/>
             ))}
         </ul>
        </div>
