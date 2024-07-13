c7a4a2e8fdc1fb6f3aef6c784c8a5006228fe1dd
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-07-13 15:05:52 +0300

    changed project's structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import UsersPage from "./Pages/UsersPage/UsersPage";
+import PostsPage from "./Pages/PostsPage/PostsPage";
+import CommentsPage from "./Pages/CommentsPage/CommentsPage";
 
 
 
@@ -10,7 +13,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 let router = createBrowserRouter([
-    {path:'/', element: <MainLayout/>}
+    {path:'/', element: <MainLayout/>, children:[
+            {path:'users', element:<UsersPage/>},
+            {path:'posts', element:<PostsPage/>},
+            {path:'comments', element:<CommentsPage/>}
+        ]}
 ]);
 
 root.render(
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MenuComponent from "../components/MenuComponent/MenuComponent";
 import styles from "./MainLayout.module.css"
+import {Outlet} from "react-router-dom";
 
 const MainLayout = () => {
     return (
         <div className={styles.mainDiv}>
             <MenuComponent/>
-            main layout
+            <Outlet/>
         </div>
     );
 };
